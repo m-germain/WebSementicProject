ae8faaae32dd52e2e41877fcebed760be736462c
corentin laharotte <colapala@gmail.com>
2019-11-12 11:58:41 +0100

    add get parameter

--- API/app.py
+++ API/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/recette')
+def getRecette():
+    response = request.args
+    ingredient = response['ingredient']
+    note = response['note']
+    tempDePrep = response['tempDePrep']
+    difficulty = response['difficulty']
+    return recetteName
 
 
 if __name__ == '__main__':
